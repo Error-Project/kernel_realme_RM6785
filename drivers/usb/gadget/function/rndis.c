52766d0f935598abf0de9bcb11e2f4d59e769944
anupamroy777 <github.769@gmail.com>
2022-03-31 08:23:01 +0000

    Revert "usb: gadget: rndis: add spinlock for rndis response list"
    This reverts commit 669c2b178956718407af5631ccbc61c24413f038.

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -1140,7 +1140,6 @@ static rndis_resp_t *rndis_add_response(struct rndis_params *params, u32 length)
 	r->buf = (u8 *)(r + 1);
 	r->length = length;
 	r->send = 0;
-
 	list_add_tail(&r->list, &(params->resp_queue));
 	return r;
 }
